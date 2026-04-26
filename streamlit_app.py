be2d215f9840b16da3c4ee5708b850c80854c96c
shinichiromizuno <45525684+shinichiromizuno@users.noreply.github.com>
2026-04-26 23:13:29 +0900

    Parse and display JSON response for watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
         
